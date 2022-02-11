13c1ae4879a97fb02af28eb91c423ee5e348219f
seilk <2018141123@yonsei.ac.kr>
2022-02-11 14:38:11 +0900

    코로나 확진자 정보 수정

--- Corona.js
+++ Corona.js
@@ -1,18 +1,15 @@
 function getCorona(){
-  var a = org.jsoup.Jsoup.connect("http://m.naver.com").get();
-  var b = a.select(".cbc_term").text().split(" ")
-  var c = a.select(".cbc_number").text().split(" ")
+  var a = org.jsoup.Jsoup.connect("https://m.news.naver.com/covid19/index#infection-status").get();
+  var b = a.select(".desc").text().toString().split(" ")
+  var c = b[2].replace(/[/[ㄱ-ㅎㅏ-ㅣ가-힣]/g,"") //확진자 수
+  var d = b[4]+"명 "+b[5] //증가, 감소분
   var results = [];
   var dayInfo = getDate();
   results[0] = "<<"+dayInfo.join(".")+">>"
-  results[1] = "🔴신규확진자 : "+c[0]+"명";
-  results[2] = "💉2차 접종률 : "+c[1];
-  results[3] = "💉3차 접종률 : "+c[2];
+  results[1] = "🔴신규확진자 : "+c+"명";
+  results[2] = "🟠어제보다 "+d
+  results[3] = "💉2차 접종률 : "+b[0];
+  results[4] = "💉3차 접종률 : "+b[1];
   var res = results.join("\n");
   return res
-}
-if(dict_cmd[msg]=="/코로나"){
-  var res = getCorona();
-  replier.reply(res);
-  return
 }
